3954ebc89e6a9ba3533b0a3f64d155aa9a03cfb7
DavidTG7 <davidthekoa@gmail.com>
2022-05-20 23:07:41 -0500

    Update styles

--- js/app.js
+++ js/app.js
@@ -11,7 +11,21 @@ const fetchData = async(id) => {
   try {
     const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id)
     const data = await res.json()
-    pintarCard(data)
+    console.log(data);
+    
+    const pokemon = {
+      img: data.sprites.other.dream_world.front_default,
+      name: data.name,
+      hp: data.stats[0].base_stat,
+      experience: data.base_experience,
+      attack: data.stats[1].base_stat,
+      special: data.stats[3].base_stat,
+      defense: data.stats[2].base_stat,
+
+
+
+    }
+    pintarCard(pokemon)
   } catch (error) {
     console.log(error);
   }
@@ -20,14 +34,20 @@ const fetchData = async(id) => {
 
 const pintarCard = (pokemon) => {
   
-  console.log(pokemon);
 
   const flex = document.querySelector(".flex")
   const template = document.querySelector("#template-card").content
   const clone = template.cloneNode(true)
   const fragment = document.createDocumentFragment()
 
-  clone.querySelector(".card-body-img").setAttribute("src", pokemon.sprites.other.dream_world.front_default)
+  clone.querySelector(".card-body-img").setAttribute("src", pokemon.img)
+  clone.querySelector(".card-body-title").innerHTML = `${pokemon.name} <span>${pokemon.hp}Hp</span>`
+  clone.querySelector(".card-body-text").textContent = pokemon.experience + ' Exp'
+  clone.querySelectorAll(".card-footer-social h3")[0].textContent = pokemon.attack + "K"
+  clone.querySelectorAll(".card-footer-social h3")[1].textContent = pokemon.special + "K"
+  clone.querySelectorAll(".card-footer-social h3")[2].textContent = pokemon.defense + "K"
+
+
 
   fragment.appendChild(clone)
   flex.appendChild(fragment)
